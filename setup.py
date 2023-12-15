4956431061905d207556f60e4458c3aaa0c7c2d5
Den Mikhailenko <den.mko@gmail.com>
2023-12-15 18:21:05 +0200

    Changed meta for HA tests

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ setup(
         "boto3>=1.20.24",
         "requests>=2.28.1",
     ],
-    version="0.41.0",
+    version="0.41.2",
     description="A library to communicate with Switchbot",
     author="Daniel Hjelseth Hoyer",
-    url="https://github.com/Danielhiversen/pySwitchbot/",
+    url="https://github.com/den-mko/pySwitchbot/",
     license="MIT",
     classifiers=[
         "Development Status :: 3 - Alpha",
